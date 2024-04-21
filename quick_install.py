2b3f052cea33e95332f464983c8ddcc53614b8a5
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2024-04-22 01:54:16 +0200

    better installation script, in python

--- quick_install.py
+++ quick_install.py
@@ -2,7 +2,6 @@ import pyperclip
 import os
 
 
-# todo: extend install_chat_command.sh to ask for key and optially write it to .bashrc
 INSTALL_STRING = f"""sudo apt update
 sudo apt install git-all
 git clone https://github.com/mikhail-vlasenko/chat-command.git
@@ -10,15 +9,14 @@ cd chat-command
 sudo apt install python3-pip
 pip3 install -r requirements.txt
 export CHAT_COMMAND_PATH="$HOME/.chat_command"
-source install_chat_command.sh
-echo "export CHAT_COMMAND_PATH="$CHAT_COMMAND_PATH >> ~/.bashrc
-echo "export CHAT_COMMAND_PYTHON="$(which python3) >> ~/.bashrc
-echo "source "$CHAT_COMMAND_PATH"/chat_wrapper.sh" >> ~/.bashrc
-echo "export OPENAI_API_KEY="{os.getenv('OPENAI_API_KEY')} >> ~/.bashrc
+export OPENAI_API_KEY="{os.getenv('OPENAI_API_KEY')}"
+python3 setup.py --persistent --skip-alias
+cd ..
+rm -rf chat-command
 source ~/.bashrc
 echo -e "\nInstallation complete. You can now use the chat command. \\U1F916"
 """
 
 if __name__ == "__main__":
     pyperclip.copy(INSTALL_STRING)
-    print("Installation string is copied to clipboard.")
+    print("📋 Installation string is copied to clipboard.")
---
+++ setup.py
@@ -0,0 +1,119 @@
+import os
+import shutil
+import re
+import argparse
+
+
+def check_and_set_chat_command_path(args_path):
+    chat_command_path = os.getenv('CHAT_COMMAND_PATH')
+    if args_path:
+        chat_command_path = args_path
+
+    if not chat_command_path:
+        default_path = os.path.expanduser("~/.chat_command")
+        chat_command_path = input(
+            f"Define CHAT_COMMAND_PATH or Enter for [{default_path}]: ") or default_path
+        os.environ['CHAT_COMMAND_PATH'] = chat_command_path
+
+    if not os.path.exists(chat_command_path):
+        os.makedirs(chat_command_path)
+        os.makedirs(os.path.join(chat_command_path, "chat_history"))
+    return chat_command_path
+
+
+def copy_script_contents(chat_command_path):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def ignore_dot_files(directory, contents):
+        return [filename for filename in contents if filename.startswith('.')]
+
+    shutil.copytree(script_dir, chat_command_path, ignore=ignore_dot_files, dirs_exist_ok=True)
+
+
+def check_and_set_api_key():
+    api_key = os.getenv('OPENAI_API_KEY')
+    if not api_key:
+        api_key = input("OPENAI_API_KEY is not set. Please provide it: ")
+        os.environ['OPENAI_API_KEY'] = api_key
+    return api_key
+
+
+def update_shell_config(chat_command_path, api_key, persistent_flag=False, skip_alias=False):
+    shell_config_options = {
+        '1': '~/.bashrc',
+        '2': '~/.zshrc',
+        '3': 'Enter custom path',
+        '4': 'Skip this step'
+    }
+
+    if persistent_flag:
+        config_path = os.path.expanduser(shell_config_options.get('1', ''))
+    else:
+        print("Choose your shell configuration file for persistent installation:")
+        for key, value in shell_config_options.items():
+            print(f'{key}: {value}')
+        choice = input("Your choice ([1]-4): ") or '1'
+        if choice == '3':
+            config_path = input("Enter the path to your shell configuration file: ")
+            config_path = os.path.expanduser(config_path)
+        elif choice == '4':
+            return
+        else:
+            config_path = os.path.expanduser(shell_config_options.get(choice, ''))
+
+    settings = [
+        f'export CHAT_COMMAND_PATH="{chat_command_path}"',
+        f'export CHAT_COMMAND_PYTHON="$(which python3)"',
+        f'source $CHAT_COMMAND_PATH"/chat_wrapper.sh"',
+        f'export OPENAI_API_KEY="{api_key}"'
+    ]
+    if not skip_alias:
+        settings.append('chat_install() { python3 $CHAT_COMMAND_PATH/quick_install.py }')
+
+    with open(config_path, 'a+') as file:
+        file.seek(0)
+        existing_content = file.read()
+        for setting in settings:
+            if setting.startswith('export'):
+                # Regular expression to check if the variable name is declared
+                var_name = re.escape(re.split(r'[ =]', setting)[1])
+                pattern = re.compile(r'^\s*export\s+' + var_name, re.MULTILINE)
+            else:
+                pattern = re.compile(re.escape(setting), re.MULTILINE)
+
+            if not re.search(pattern, existing_content):
+                file.write(setting + '\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Set up the chat command environment.")
+    parser.add_argument("--chat_command_path",
+                        help="Set the CHAT_COMMAND_PATH environment variable")
+    parser.add_argument("--api_key",
+                        help="Set the OPENAI_API_KEY environment variable")
+    parser.add_argument("--persistent",
+                        help="Automatically select the default shell configuration without prompt",
+                        action='store_true')
+    parser.add_argument("--skip_alias",
+                        help="Skip adding the chat_install alias to the shell configuration",
+                        action='store_true')
+    args = parser.parse_args()
+
+    chat_command_path = check_and_set_chat_command_path(args.chat_command_path)
+    copy_script_contents(chat_command_path)
+
+    if args.api_key:
+        api_key = args.api_key
+        os.environ['OPENAI_API_KEY'] = api_key
+    else:
+        api_key = check_and_set_api_key()
+
+    update_shell_config(chat_command_path, api_key, args.persistent, args.skip_alias)
+    print(f'Installation complete. '
+          f'You may need to restart your shell or run\n'
+          f'source "{chat_command_path}/chat_wrapper.sh"\n'
+          f'to use the chat command.')
+
+
+if __name__ == "__main__":
+    main()
